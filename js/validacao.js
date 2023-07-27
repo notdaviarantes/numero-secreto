d126d155db98ae8a61c6cb2aab982daa5cf55013
notdaviarantes <daviarantes13@gmail.com>
2023-07-27 15:34:22 -0300

    adicionando um botão para jogar novamente

--- js/validacao.js
+++ js/validacao.js
@@ -15,6 +15,7 @@ function validaChute(chute) {
     document.body.innerHTML = `
       <h2>Você acertou!!</h2> 
       <h3>O número secreto era ${numeroSecreto}</h3>
+      <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
     `;
   } else if (numero > numeroSecreto) {
     elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
@@ -22,3 +23,9 @@ function validaChute(chute) {
     elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
   }
 }
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.id == 'jogar-novamente') {
+    window.location.reload();
+  }
+});
